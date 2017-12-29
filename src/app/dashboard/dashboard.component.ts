3d512d849ef4b5f7a0060cdbb331b9cfc0beb86d
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2017-12-29 10:02:57 -0600

    css -> scss + dashboard generated

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kb-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
