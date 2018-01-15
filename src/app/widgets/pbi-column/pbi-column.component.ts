e34625c891e5f8ecdf5d8ec8153b9fb2ce4aef3d
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2018-01-15 17:00:40 -0600

    Display pbis in order of backlog priority

--- src/app/widgets/pbi-column/pbi-column.component.ts
+++ src/app/widgets/pbi-column/pbi-column.component.ts
@@ -1,18 +1,23 @@
 import { WorkItem } from './../../models/work-item';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'kb-pbi-column',
     templateUrl: './pbi-column.component.html',
     styleUrls: ['./pbi-column.component.scss']
 })
-export class PbiColumnComponent implements OnInit {
+export class PbiColumnComponent implements OnChanges {
     @Input() list: Array<WorkItem> = new Array<WorkItem>();
     @Input() label: string;
 
     constructor() { }
 
-    ngOnInit() {
+    ngOnChanges() {
+        if (this.list) {
+            // Order pbi list
+            this.list = this.list.sort((a: WorkItem, b: WorkItem) => {
+                return a.backlogPriority - b.backlogPriority;
+            });
+        }
     }
-
 }
