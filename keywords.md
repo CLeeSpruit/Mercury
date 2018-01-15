cd05c03219fdcf91cfba325070954821356ccccf
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2018-01-15 16:53:19 -0600

    Added colors because I am tired of looking at monochrome

---
+++ keywords.md
@@ -0,0 +1,12 @@
+Ranked by Priority
+
+* 'PUBLISH' - fa-paper-plane TODO: Okay, nevermind bomb is much better
+* 'TRAC' - fa-handshake-o     TODO: Trac logo
+* 'DOC' - fa-file-text-o    TODO: Docman
+* 'CM' - fa-user-circle-o
+* 'VM' - fa-users
+* 'BUG' - fa-bug
+* 'Tech Debt', 'R & D', 'R&D' - fa-flask
+* 'OTIS' - fa-lemon
+* 'OP', 'OPUS' - fa-superpowers TODO: idk
+* Anything else - Cogency Global logo
--- src/app/widgets/pbi-card/pbi-card.component.ts
+++ src/app/widgets/pbi-card/pbi-card.component.ts
@@ -1,17 +1,33 @@
 import { WorkItem } from './../../models/work-item';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'kb-pbi-card',
     templateUrl: './pbi-card.component.html',
     styleUrls: ['./pbi-card.component.scss']
 })
-export class PbiCardComponent implements OnInit {
+export class PbiCardComponent implements OnChanges {
     @Input() pbi: WorkItem;
+    keyword: string;
 
     constructor() { }
 
-    ngOnInit() {
+    ngOnChanges() {
+        if (this.pbi) {
+            this.parseHeader();
+        }
     }
 
+    private parseHeader() {
+        const title = this.pbi.title;
+        if (title.toLowerCase().includes('publish')) {
+            this.keyword = 'publish';
+        } else if (title.toLowerCase().includes('trac')) {
+            this.keyword = 'trac';
+        } else if (title.toLowerCase().includes('r & d')) {
+            this.keyword = 'test';
+        } else {
+            this.keyword = 'op';
+        }
+    }
 }
