2ffca89bc46d1c07e6a2e4d5c12b00039ebd3096
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2018-02-28 17:25:41 -0600

    Create releases

--- src/app/environments/heartbeat/heartbeat.component.ts
+++ src/app/environments/heartbeat/heartbeat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TfsEnvironmentService } from '../services/tfs-environment.service';
 import { Deployment } from '../models/deployment.model';
+import { Build } from '../models/build.model';
 
 @Component({
     selector: 'hg-heartbeat',
@@ -9,7 +10,6 @@ import { Deployment } from '../models/deployment.model';
 })
 export class HeartbeatComponent implements OnInit {
     @Input() deployment: Deployment;
-    // buildData: BuildRecord;
 
     constructor(
         private tfsEnvironmentService: TfsEnvironmentService
@@ -19,5 +19,10 @@ export class HeartbeatComponent implements OnInit {
         // this.tfsEnvironmentService.getBuildTimeline(this.build).subscribe((data: BuildRecord) => {
         //     this.buildData = data;
         // });
+    }
+
+    startRelease(buildId: number) {
+        const found = this.deployment.applicableBuilds.find(build => build.id === +buildId);
+        this.tfsEnvironmentService.createRelease(found, this.deployment.release.releaseDefinition).subscribe((data) => {});
     }
 }
--- src/app/environments/services/tfs-environment.service.ts
+++ src/app/environments/services/tfs-environment.service.ts
@@ -6,7 +6,10 @@ import 'rxjs/add/operator/catch';
 
 import { Build } from '../models/build.model';
 import { Release } from '../models/release.model';
-import { Artifact } from '../models/artifact.model';
+import { Artifact, ReleaseArtifact } from '../models/artifact.model';
+import { ReleaseRequest, ReleaseArtifact } from '../models/release-request.model';
+import { BuildDefinition } from '../models/build-definition.model';
+import { ReleaseDefinition } from '../models/release-definition.model';
 
 @Injectable()
 export class TfsEnvironmentService {
@@ -20,7 +23,7 @@ export class TfsEnvironmentService {
         headers: new Headers({
             'cache-control': 'no-cache',
             'authorization': `Basic ${this.accessCode}`,
-            'Content-Type': 'application/json-patch+json'
+            'Content-Type': 'application/json'
         })
     };
 
@@ -68,16 +71,16 @@ export class TfsEnvironmentService {
                 const artifact = <Artifact>{};
                 if (response.environments && response.environments[0]
                     && response.environments[0].artifacts && response.environments[0].artifacts[0]) {
-                        if (response.environments[0].artifacts[0].version) {
-                            artifact.versionId = response.environments[0].artifacts[0].version.id;
-                            artifact.versionName = response.environments[0].artifacts[0].version.name;
-                        }
-
-                        if (response.environments[0].artifacts[0].branch) {
-                            artifact.branchId = response.environments[0].artifacts[0].branch.id;
-                            artifact.branchName = response.environments[0].artifacts[0].branch.name;
-                        }
+                    if (response.environments[0].artifacts[0].version) {
+                        artifact.versionId = response.environments[0].artifacts[0].version.id;
+                        artifact.versionName = response.environments[0].artifacts[0].version.name;
                     }
+
+                    if (response.environments[0].artifacts[0].branch) {
+                        artifact.branchId = response.environments[0].artifacts[0].branch.id;
+                        artifact.branchName = response.environments[0].artifacts[0].branch.name;
+                    }
+                }
                 return <Release>{
                     ...response,
                     artifact
@@ -96,6 +99,19 @@ export class TfsEnvironmentService {
             .catch(this.handleError);
     }
 
+    createRelease(build: Build, definition: ReleaseDefinition) {
+        return this.http
+        .post(
+            `${this.baseLocationOpus}/release/releases?api-version=2.2-preview.1`,
+            JSON.stringify(this.createReleaseRequest(build, definition)),
+            this.options)
+        .map((res: Response) => {
+            const response = res.json();
+            return response.value;
+        })
+        .catch(this.handleError);
+    }
+
     private createAuthorization(token: string) {
         const buff = new Buffer(`:${token}`);
         return buff.toString('base64');
@@ -104,5 +120,27 @@ export class TfsEnvironmentService {
     private handleError(error: Response | any, caught: Observable<any>) {
         console.error(error.json());
         return Observable.throw(error);
+    }
+
+    private createReleaseRequest(build: Build, definition: ReleaseDefinition) {
+        return <ReleaseRequest>{
+            definitionId: definition.id,
+            description: 'Mercury Task Manager',
+            artifacts: [
+                {
+                    alias: definition.name,
+                    instanceReference: {
+                        name: build.buildNumber,
+                        id: build.id.toString(),
+                        sourceBranch: build.sourceBranch,
+                        sourceVersion: build.sourceVersion,
+                        sourceRepositoryId: '27b5dc0b-2b13-49e9-b443-bde560692509', // TODO: Don't hardcode this
+                        sourceRepositoryType: 'TfsGit'
+                    }
+                }
+            ],
+            isDraft: false,
+            manualEnvironments: []
+        };
     }
 }
