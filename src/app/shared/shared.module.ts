3270b09fe37832a44cd72ea83b9cf910bdee4939
Cassandra Spruit <cassandra.spruit+github@gmail.com>
2019-03-26 11:55:10 -0500

    Added Electron Service

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { DynamicComponentService } from '@shared/services/dynamic-component.service';
 import { QueryService } from '@shared/services/query.service';
+import { ElectronService } from '@shared/services/electron.service';
 @NgModule({
     providers: [
-        DynamicComponentService
+        DynamicComponentService,
+        ElectronService
     ]
 })
 export class SharedModule { }
